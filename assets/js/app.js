3e1a39916b1d6ce3af317c617aeb45548f88c3d8
EFerri222 <eferri222@gmail.com>
2020-05-19 03:10:56 -0400

    Add functionality to nav-link class

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+var navLinks = document.querySelectorAll(".nav-link");
+
+// Add functionality to nav-tabs class
+for(var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function() {
+        for(var j = 0; j < navLinks.length; j++) {
+            navLinks[j].classList.remove("active");
+        }
+        this.classList.add("active");
+    });
+}
