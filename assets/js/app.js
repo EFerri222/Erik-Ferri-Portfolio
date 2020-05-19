6f5c760e62f3c69b1393a3d469a3556517af20f6
EFerri222 <eferri222@gmail.com>
2020-05-19 03:29:05 -0400

    Refactor comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 var navLinks = document.querySelectorAll(".nav-link");
 
-// Add functionality to nav-tabs class
+// Add functionality to nav-link class
 for(var i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener("click", function() {
         for(var j = 0; j < navLinks.length; j++) {
